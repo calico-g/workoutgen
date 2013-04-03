8113bc6b46a3d710da7d5a2603651ace004de4e5
calico-g <calico.goodrich@gmail.com>
2013-04-02 17:16:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workoutgen::Application.config.secret_token = 'eb751469a1a96451fcd2a882619eca2f36101907b75b4ebabefa1c5bc53580570cd5ab9837740d54a1ee0c8c9e8ca1d50f34fca629a6c181ea93576298bca33b'
