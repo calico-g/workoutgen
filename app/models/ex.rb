c84a2b9ca44e335a08de443c5256cb579b214a09
calico-g <calico.goodrich@gmail.com>
2013-04-04 16:25:13 -0700

    Image now shows when user views exercise

--- app/models/ex.rb
+++ app/models/ex.rb
@@ -1,4 +1,6 @@
 class Ex < ActiveRecord::Base
   attr_accessible :category, :description, :name, :url, :user_id, :asset
-  has_attached_file :asset
+  has_attached_file :asset,
+  									:url => "/assets/exes/:id/:basename.:extension",
+  									:path => ":rails_root/public/assets/exes/:id/:basename.:extension"
 end
