88099f49d2c75acc19f14ac41e111b283b15d194
calico-g <calico.goodrich@gmail.com>
2013-04-04 15:37:03 -0700

    User can view list of exercises

--- spec/integration/viewing_list_ex_spec.rb
+++ spec/integration/viewing_list_ex_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 feature 'Viewing list of exercises' do
-
+	ex1 = Factory(:ex, :name => 'pooping')
+	ex2 = Factory(:ex, :name => 'jumping')
+	ex3 = Factory(:ex, :name => 'swimming with sharks')
+	ex4 = Factory(:ex, :name => 'running with big dogs')
 
 	scenario 'can view list of exercises' do
-
+		visit '/'
+		click_link 'Exercises'
+		page.should have_content 'pooping'
+		page.should have_content 'jumping'
+		page.should have_content 'swimming with sharks'
+		page.should have_content 'running with big dogs'
 	end
 end
