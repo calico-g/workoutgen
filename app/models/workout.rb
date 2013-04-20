778be342bbb4d27265aa4a8cb41d8e890535fa52
calico-g <calico.goodrich@gmail.com>
2013-04-20 13:55:25 -0700

    Moved workout generator to model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,10 @@
 class Workout < ActiveRecord::Base
   attr_accessible :cardio, :strength, :stretch, :total
+
+  def exercises
+  	strengths = Ex.where(:category => 'strength')
+		(1..strength/2).to_a.map do
+			strengths[rand(strengths.count)]
+		end
+	end
 end
