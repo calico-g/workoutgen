3012a9a5a1e85e2e2fb7ee6934fc8cd5bff16607
calico-g <calico.goodrich@gmail.com>
2013-05-09 22:37:07 -0700

    Change host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,7 @@ Workoutgen::Application.configure do
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
+
+  config.action_mailer.default_url_options =
+    {:host => 'amazing-rando.heroku.com'}
 end
